0868a6d99003a6ee05327ee4acff85b7be3f9523
rovilay <rovilayjnr01@gmail.com>
2018-12-15 07:31:57 +0100

    feat(search, dynamic cards and pagination): search functionality and pagination
    - make card content dynamic
    - add pagination
    - search functionality
    
    [Delivers]

--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,64 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from .models import Listing
+from django.core.paginator import EmptyPage, Page, PageNotAnInteger, Paginator
+from .choices import state_choices, bedroom_choices, price_choices
 
 def index(request):
-    return render(request, 'listings/index.htm')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
-def listing(request):
-    return render(request, 'listings/listing.htm')
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    context = { 'listings': paged_listings }
+    return render(request, 'listings/index.htm', context)
+
+def listing(request, listing_id):
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    context = { 'listing': listing }
+    return render(request, 'listings/listing.htm', context)
 
 def search(request):
-    return render(request, 'listings/search.htm')
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+
+    # City
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+
+    # State
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(state__iexact=state)
+
+    # Bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+
+    # P rice
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price)
+
+
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'listings': queryset_list,
+        'values': request.GET
+    }
+    return render(request, 'listings/search.htm', context)
 
